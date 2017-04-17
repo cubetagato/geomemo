2ca1e9b53877fb685ed7326f9489155457c9c4b2
Gabriel I. Hernández G <hdzgabriel@ayanamireini.local>
2017-04-17 09:05:26 -0500

    Initial Commit

---
+++ geomemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  geomemo
+//
+//  Created by Gabriel I. Hernández G. on 17/04/17.
+//  Copyright © 2017 Gabriel I. Hernández G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
